08d94cc7fc0898f4414bb4b5ddb9e54fde012186
sultzx <quangaliyev@gmail.com>
2023-01-04 23:09:37 +0500

    last

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+
+import './index.css'
+import App from './App'
+import store from './redux/store.js'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
