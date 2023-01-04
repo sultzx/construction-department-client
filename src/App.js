265bacf36116902f069dda9204ebe1279787e849
sultzx <quangaliyev@gmail.com>
2023-01-05 04:17:43 +0500

    last

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
 
+// import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth";
 
 function App() {
+
+  // const dispatch = useDispatch()
+
+  // const isAuth = useSelector(selectIsAuth)
+
+  // React.useEffect(() => {
+  //   dispatch(fetchAuthMe())
+  // }, [])
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/login' element={<>Login</>}/>
+        <Route path='/registration' element={<>Registration</>}/>
+        <Route path='/' element={<>Main</>}/>
+      </Routes>
+    </>
   );
 }
 
