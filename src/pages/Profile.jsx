33dade019a05ce6d14d604262ad30ba60003ec11
sultzx <quangaliyev@gmail.com>
2023-01-11 23:25:31 +0500

    last

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -12,10 +12,13 @@ import { useSelector } from "react-redux";
 
 import { selectIsAuth } from "../redux/slices/auth";
 import "../styles/index.scss";
-import "../styles/Profile.scss"
+import "../styles/Profile.scss";
+import blueProfile from "../images/blue-profile.png";
 
 const Profile = () => {
   const isAuth = useSelector(selectIsAuth);
+
+  const userData = useSelector((state) => state.auth.data);
 
   return (
     <>
@@ -56,19 +59,32 @@ const Profile = () => {
           </Breadcrumb>
           <hr className="basic-hr" />
           <Row>
-            <Col lg={4} md={4} sm={6} xs={12}>
-              <Card className="profile-page-card">
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+            <Col lg={4} md={6} sm={6} xs={12}>
+              <Card className="profile-page-card shadow">
+                <Card.Body 
+                  style={{height: '460px'}}
+                  className=" d-flex d-sm-flex d-md-flex d-lg-flex d-xl-flex d-xxl-flex flex-column justify-content-center align-items-center justify-content-sm-center align-items-sm-center justify-content-md-center align-items-md-center justify-content-lg-center align-items-lg-center justify-content-xl-center align-items-xl-center justify-content-xxl-center align-items-xxl-center ">
+                  <div className="circular--landscape">
+                  <Card.Img
+                    src={
+                      userData && userData.avatarUrl
+                        ? userData.avatarUrl
+                        : blueProfile
+                    }
+                  />
+                  </div>
+                  
+                  <Card.Title className="text-center" style={{
+                    color: '#0E6BA8'
+                  }}>
+                    {userData && userData.lastname ? userData.lastname : ""} {" "}
+                    {userData && userData.firstname ? userData.firstname : " "} {" "} <br />
+                    {userData && userData.patronymic ? userData.patronymic : " "}
+                  </Card.Title>
                 </Card.Body>
               </Card>
             </Col>
-            <Col lg={8} md={8} sm={6} xs={12}>
+            <Col lg={8} md={6} sm={6} xs={12}>
               <Card className="profile-page-card">
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
