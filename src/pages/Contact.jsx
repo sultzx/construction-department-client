5b60ba295ed8ee5ae53f926e532c06e85d402b21
sultzx <quangaliyev@gmail.com>
2023-01-11 19:50:48 +0500

    last

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,39 +3,53 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import { fetchAuth, selectIsAuth } from "../redux/slices/auth.js";
+import "../styles/index.scss";
 
 const Contact = () => {
-
-  const isAuth = useSelector(selectIsAuth)
+  const isAuth = useSelector(selectIsAuth);
 
   return (
     <>
       <Alert
         variant={!isAuth ? "warning" : "primary"}
         style={
-            !isAuth
+          !isAuth
             ? { borderRadius: "1px", borderColor: "yellow" }
             : { borderRadius: "1px" }
-        }>
+        }
+      >
         {
           <div className="text-center" style={{ margin: "-12px" }}>
-            {!isAuth && <span>Жүйедегі толық ақпараттарға қол жеткізу үшін тіркеліңіз немесе кіріңіз</span>}
+            {!isAuth && (
+              <span>
+                Жүйедегі толық ақпараттарға қол жеткізу үшін тіркеліңіз немесе
+                кіріңіз
+              </span>
+            )}
           </div>
         }
       </Alert>
       <Container>
-      <Breadcrumb style={{ background: "transparent"}}>
-            <Breadcrumb.Item >
-              <Link to={"/main"}  style={{color: '#267DB5', fontSize: '16px'}}>Басты бет</Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item active="true" style={{color: '#267DB5', fontSize: '16px'}}>Бізбен байланыс</Breadcrumb.Item>
+        <Breadcrumb className="breadcrumb-component">
+          <Breadcrumb.Item>
+            <Link to={"/main"} className="breadcrumb-component-item">
+              Басты бет
+            </Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item
+            active="true"
+            className="breadcrumb-component-item"
+            style={{ color: "#267DB5" }}
+          >
+            Бізбен байланыс
+          </Breadcrumb.Item>
         </Breadcrumb>
+        <hr className="basic-hr" />
         <Row>
-            <Col lg={4} md={4} sm={6} xs={12}></Col>
-            <Col lg={8} md={8} sm={6} xs={12}></Col>
+          <Col lg={4} md={4} sm={6} xs={12}></Col>
+          <Col lg={8} md={8} sm={6} xs={12}></Col>
         </Row>
       </Container>
-
     </>
   );
 };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,60 +1,84 @@
-import { Container, Row, Col, Button, Breadcrumb, Alert, Card } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Breadcrumb,
+  Alert,
+  Card,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+
 import { selectIsAuth } from "../redux/slices/auth";
+import "../styles/index.scss";
+import "../styles/Profile.scss"
 
 const Profile = () => {
-
-    const isAuth = useSelector(selectIsAuth)
+  const isAuth = useSelector(selectIsAuth);
 
   return (
     <>
       <section>
-      <Alert
-        variant={!isAuth ? "warning" : "primary"}
-        style={
+        <Alert
+          variant={!isAuth ? "warning" : "primary"}
+          style={
             !isAuth
-            ? { borderRadius: "1px", borderColor: "yellow" }
-            : { borderRadius: "1px" }
-        }>
-        {
-          <div className="text-center" style={{ margin: "-12px" }}>
-            {!isAuth && <span>Жүйедегі толық ақпараттарға қол жеткізу үшін тіркеліңіз немесе кіріңіз</span>}
-          </div>
-        }
-      </Alert>
+              ? { borderRadius: "1px", borderColor: "yellow" }
+              : { borderRadius: "1px" }
+          }
+        >
+          {
+            <div className="text-center" style={{ margin: "-12px" }}>
+              {!isAuth && (
+                <span>
+                  Жүйедегі толық ақпараттарға қол жеткізу үшін тіркеліңіз немесе
+                  кіріңіз
+                </span>
+              )}
+            </div>
+          }
+        </Alert>
         <Container>
-          <Breadcrumb style={{ background: "transparent", height: '26px'}}>
-            <Breadcrumb.Item >
-              <Link to={"/main"}  style={{color: '#267DB5', fontSize: '16px'}}>Басты бет</Link>
+          <Breadcrumb className="breadcrumb-component">
+            <Breadcrumb.Item>
+              <Link to={"/main"} className="breadcrumb-component-item">
+                Басты бет
+              </Link>
             </Breadcrumb.Item>
-            <Breadcrumb.Item active="true" style={{color: '#267DB5', fontSize: '16px'}}>Жеке профиль</Breadcrumb.Item>
+            <Breadcrumb.Item
+              active="true"
+              className="breadcrumb-component-item"
+              style={{ color: "#267DB5" }}
+            >
+              Жеке профиль
+            </Breadcrumb.Item>
           </Breadcrumb>
-          <hr style={{ color: '#0C488D' }}/>
+          <hr className="basic-hr" />
           <Row>
             <Col lg={4} md={4} sm={6} xs={12}>
-            <Card className="profile-page-card">
+              <Card className="profile-page-card">
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
-            </Card>
+              </Card>
             </Col>
             <Col lg={8} md={8} sm={6} xs={12}>
-            <Card className="profile-page-card">
+              <Card className="profile-page-card">
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
-            </Card>
+              </Card>
             </Col>
           </Row>
         </Container>
